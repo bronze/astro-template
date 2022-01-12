faaeca21e103098b2a4ac5af8df0272925755487
Carlos Bronze <170220+bronze@users.noreply.github.com>
2022-01-12 02:16:26 -0300

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+// https://github.com/withastro/astro/blob/latest/examples/docs/src/config.ts
+export const SITE = {
+  title: 'Config Title',
+  description: 'Config Description',
+  defaultLanguage: 'en_US',
+};
+export const OPEN_GRAPH = {
+  image: {
+    src: 'https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true',
+    alt: 'astro logo on a starry expanse of space,' + ' with a purple saturn-like planet floating in the right foreground',
+  },
+  twitter: 'astrodotbuild',
+};
---
+++ src/utils.ts
@@ -0,0 +1,31 @@
+// https://gist.github.com/Princesseuh/43e3fde64bff62a469c96810d02e0623
+import Image from "@11ty/eleventy-img"
+
+export function generateImage(
+  src: string,
+  options,
+  addBasePath = true,
+): Record<string, ImageFormat[]> {
+  const settings = Object.assign(options, {
+    outputDir: "public/assets/images",
+    urlPath: "/assets/images",
+  })
+  src = (addBasePath ? "src/assets" : "") + src
+  ;(async () => {
+    await Image(src, settings)
+  })()
+
+  return Image.statsSync(src, settings)
+}
+
+export interface ImageFormat {
+  format: string
+  width: number
+  height: number
+  filename: string
+  outputPath: string
+  url: string
+  sourceType: string
+  srcset: string
+  size: number
+}
